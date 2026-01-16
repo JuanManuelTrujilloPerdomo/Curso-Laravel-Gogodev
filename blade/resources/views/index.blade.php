2f5e1774d7921e994875abb47fef73166d7a6b74
Juan Manuel Trujillo <Juanperdomotorre.13@gmail.com>
2026-01-16 17:44:15 -0500

    Directivas de blade para reutilizar y extender codigo HTML

--- blade/resources/views/index.blade.php
+++ blade/resources/views/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-</head>
-<body>
-    <h1>Index</h1>
-</body>
-</html>
+@extends('layouts.landing') {{--Se utiliza la directiva para traer el HTML del layouts del landing --}}
+
+@section('title', 'Home') {{--Seccion la cual inyectamos en el layouts de landing --}}
+    
+@section('content')
+<h1>Index</h1>
+@endsection
