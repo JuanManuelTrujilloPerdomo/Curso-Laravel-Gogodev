30a3871b1ff2d829c304cad07fe092d824a0cf7d
Juan Manuel Trujillo <Juanperdomotorre.13@gmail.com>
2026-01-16 18:16:45 -0500

    Directivas de blade para utilizar rutas y enlaces en los menus

--- blade/resources/views/layouts/_partials/menu.blade.php
+++ blade/resources/views/layouts/_partials/menu.blade.php
@@ -1,10 +1,10 @@
-    <header>
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <ul>
+            <li><a href="{{ route('index')}}">Home</a></li> {{--Metodo de Route que nos ayuda a navegar en las rutas utilizando doble llave ({{ }}). --}}
+            <li><a href="{{ route('about')}}">About</a></li>
+            <li><a href="{{ route('services')}}">Services</a></li>
+            <li><a href="{{ route('contact')}}">Contact</a></li>
+        </ul>
+    </nav>
+</header>
