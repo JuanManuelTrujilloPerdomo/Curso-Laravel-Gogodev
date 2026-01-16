ff8224dad4a9e8a1233f084e556a809ec08c2b0a
Juan Manuel Trujillo <Juanperdomotorre.13@gmail.com>
2026-01-16 16:23:14 -0500

    Hola Mundo

--- hello_world/routes/web.php
+++ hello_world/routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','landing.index')->name('index'); /*En laravel la raiz es el directorio view, y se llama la capeta y con el punto (.) el archivos */
+Route::view('/about','landing.about')->name('about');
