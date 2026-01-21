82a51026092d05eaef1354a0b628ec21e18f4317
Juan Manuel Trujillo <Juanperdomotorre.13@gmail.com>
2026-01-20 23:13:11 -0500

    Implementando el método asset para contenido estático

--- blade/resources/views/_components/card.blade.php
+++ blade/resources/views/_components/card.blade.php
@@ -1,4 +1,5 @@
 <div style="border: 1px solid black; border-radius: 5px; padding: 10px; margin: 5px;">
     <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/Extensiones.png')}}" alt="example" width="228">
     <p>{{ $content }}</p>
 </div>
