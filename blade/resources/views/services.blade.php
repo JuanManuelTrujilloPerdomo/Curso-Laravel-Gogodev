8f15ae133eb60a6b7e7daecdf748f7c1b879ae07
Juan Manuel Trujillo <Juanperdomotorre.13@gmail.com>
2026-01-20 22:48:59 -0500

    Implementando los Componentes de blade en los Services utilizando una Card

--- blade/resources/views/services.blade.php
+++ blade/resources/views/services.blade.php
@@ -4,14 +4,28 @@
 
 @section('content')
     <h1>Services</h1>
+    {{-- Opcion 1 para implementarlo --}}
     @component('_components.card')
         @slot('title', 'Services 1')
+        @slot('content', 'Lorem Ipsum')
+    @endcomponent
+    {{-- Opcion 2 para implementarlo --}}
+    @component('_components.card')
+        @slot('title', 'Services 2')
         @slot('content')
             <h3>Example</h3>
             <p>Lorem Ipsum</p>
         @endslot
-
-    @endslot
-@endcomponent
-@endcomponent
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Services 3')
+        @slot('content')
+            <h3>Example</h3>
+            <p>Lorem Ipsum</p>
+        @endslot
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Services 4')
+        @slot('content', 'Lorem Ipsum')
+    @endcomponent
 @endsection
