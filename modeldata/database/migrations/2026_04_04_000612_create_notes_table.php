0f338e2cfe70a057aef4b64b2f517f8a0c05fff4
Juan Manuel Trujillo <juanperdomotorre.13@gmail.com>
2026-04-03 21:20:29 -0500

    Migraciones con Laravel

---
+++ modeldata/database/migrations/2026_04_04_000612_create_notes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('description', 225)->nullable(); // Quiere decir que puede ir Nul
+            $table->boolean('done')->default(false); // Si no tiene un dato se asigna por defecto valor false
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
