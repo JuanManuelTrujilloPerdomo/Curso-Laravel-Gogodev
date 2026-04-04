583a748bf31f661a19525ba137821089ca25975b
Juan Manuel Trujillo <juanperdomotorre.13@gmail.com>
2026-04-04 15:42:19 -0500

    Manejo de datos en vistas y uso de foreach

--- controllers/app/Http/Controllers/UserController.php
+++ controllers/app/Http/Controllers/UserController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index ()
     {
-        return view('user.index');
+        // Este metodo me trae todos los registros
+        $users = User::all();
+
+        // Este le pasa un array con los datos de la variable users a la vista
+        return view('user.index', compact('users'));
     }
 }
--- controllers/resources/views/user/index.blade.php
+++ controllers/resources/views/user/index.blade.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Users</title>
 </head>
+
 <body>
-    <h1>Hello World</h1>
+    <h1>Users List:</h1>
+    @if ($users->isEmpty())
+        <p>The user list is empty</p>
+    @else
+        <ul>
+            {{--  Llamamos una directiva de blade que nos permite recorrer en este caso una estrctura de datos --}}
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    {{-- Otra forma de hacer un if else --}}
+    @forelse($users as $user)
+        <li>{{ $user->name }}</li>
+    @empty
+        <p>The user list is empty</p>
+    @endforelse
+
 </body>
+
 </html>
