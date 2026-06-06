c0a7fb64064fd4f3ffb5b9aa1fdc6db238d1d064
Juan Manuel Trujillo Perdomo <juanperdomotorre.13@gmail.com>
2026-06-06 18:56:38 -0500

    Construccion de controlador API

--- api_crud/routes/api.php
+++ api_crud/routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController; 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-
+// Ruta de API para un CRUD
+Route::resource('/note', NoteController::class);
