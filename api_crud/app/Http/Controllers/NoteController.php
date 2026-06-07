1951316c1e6638702a2fca9a232e54ee0d4a7363
Juan Manuel Trujillo Perdomo <juanperdomotorre.13@gmail.com>
2026-06-06 19:03:27 -0500

    Refactorización del codigo en controlador API

--- api_crud/app/Http/Controllers/NoteController.php
+++ api_crud/app/Http/Controllers/NoteController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Note;
 use App\Http\Requests\NoteRequest;
+use Illuminate\Http\JsonResponse;
 
 class NoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index():JsonResponse
     {
-        $notes = Note:: all();
-        return response()->json($notes, 200);
+        return response()->json(Note::all(), 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function store(NoteRequest $request)
+    public function store(NoteRequest $request):JsonResponse
     {
         Note::created($request->all());
         return response()->json([
@@ -28,19 +21,12 @@ class NoteController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(string $id):JsonResponse
     {
-        $note = Note::find($id);
-        return response()->json($note, 200);
+        return response()->json(Note::all(), 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(NoteRequest $request, string $id)
+    public function update(NoteRequest $request, string $id):JsonResponse
     {
         $note = Note::find($id);
         $note->title = $request->titile;
@@ -51,10 +37,7 @@ class NoteController extends Controller
         ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(string $id):JsonResponse
     {
         Note::find($id)->delete();
         return response()->json([
